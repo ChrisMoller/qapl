b8f581d0b244d43e2dc67d69ec73f7b51382cb3d
ChrisMoller <moller@mollerware.com>
2021-10-10 13:27:18 -0400

    forgot to add...

---
+++ optionstrings.h
@@ -0,0 +1,9 @@
+#ifndef OPTIONSTRINGS_H
+#define OPTIONSTRINGS_H
+
+#define OPT_noCONT	"noCONT"
+#define OPT_noSETUP	"noSETUP"
+#define OPT_noINIT	"noINIT"
+#define OPT_L		"L"
+
+#endif // OPTIONSTRINGS_H
