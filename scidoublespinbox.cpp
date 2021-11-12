f0e0eae639f76f4efd54d76350cc26da9d09cea1
ChrisMoller <moller@mollerware.com>
2021-11-12 14:07:23 -0500

    new scientific notation spinbox w/ bells, whistles

---
+++ scidoublespinbox.cpp
@@ -0,0 +1,143 @@
+
+#include <values.h>
+
+#include "enums.h"
+#include "scidoublespinbox.h"
+
+#ifndef toCString
+#define toCString(v) ((v).toStdString ().c_str ())
+#endif
+
+#define MOUSE_BUTTON_DELAY 100
+
+SciDoubleSpinBox::SciDoubleSpinBox (SpinType t, QWidget *parent)
+  : QAbstractSpinBox (parent)
+{
+  stype = t;
+  value = 0.0;
+  timer = new QTimer(this);
+  connect (timer, &QTimer::timeout,
+	   [=]() {
+	     value += incr;
+	     this->lineEdit ()->setText (QString::number (value));
+	     Q_EMIT valueChanged ();
+	   });
+
+  //  this->setFocusPolicy (Qt::StrongFocus);
+  
+  QLineEdit *line = new QLineEdit ();
+  this->setLineEdit (line);
+  this->lineEdit ()->setText (QString::number (value));
+  this->setAccelerated (true);
+  this->setAlignment (Qt::AlignHCenter);
+  this->setMinimumWidth (100);
+  connect (this, &QAbstractSpinBox::editingFinished,
+	   [=]() {
+	     value = (this->lineEdit ()->text ()).toFloat ();
+	     Q_EMIT valueChanged ();
+	   });
+  this->installEventFilter(this);
+}
+
+double SciDoubleSpinBox::getValue ()
+{
+#if 1
+  return value;
+#else
+  double rc;
+  if (lockBox->checkState () == Qt::Unchecked)
+    rc = (stype == Min) ? -MAXDOUBLE : MAXDOUBLE;
+  else
+    rc = value;
+  return rc;
+#endif
+}
+
+#if 0
+double SciDoubleSpinBox::clampValue (double v)
+{
+  if (lockBox->checkState () == Qt::Unchecked)
+    value = v;
+  this->lineEdit ()->setText (QString::number (value));
+  return value;
+}
+#endif
+
+void SciDoubleSpinBox::keyPressEvent(QKeyEvent *keyEvent)
+{
+  bool handled = false;
+  //  Qt::KeyboardModifiers mods = keyEvent->modifiers ();
+  switch(keyEvent->key()) {
+  case Qt::Key_PageUp:
+    value += 10.0;
+    handled = true;
+    break;
+  case Qt::Key_PageDown:
+    value -= 10.0;
+    handled = true;
+    break;
+  case Qt::Key_Up:
+    value += 1.0;
+    handled = true;
+    break;
+  case Qt::Key_Down:
+    value -= 1.0;
+    handled = true;
+    break;
+  }
+  if (handled) {
+    this->lineEdit ()->setText (QString::number (value));
+    Q_EMIT valueChanged ();
+    //    lockBox->setCheckState (Qt::Checked);
+  }
+  else
+    QAbstractSpinBox::keyPressEvent(keyEvent);
+}
+
+void SciDoubleSpinBox::mousePressEvent(QMouseEvent *mouseEvent)
+{
+  bool handled = false;
+  if (mouseEvent->button () == Qt::LeftButton) {
+    int y = mouseEvent->pos ().y ();
+    int hgt = this->height ();
+    incr = (y < hgt / 2) ? 1.0 : -1.0;
+
+    value += incr;
+    handled = true;
+    mousePressActive = true;
+  }
+  if (handled) {
+    this->lineEdit ()->setText (QString::number (value));
+    Q_EMIT valueChanged ();
+    // fixme wait 1 sec.  if mouse button still active, repeat
+    QTime dieTime= QTime::currentTime().addSecs(1);
+    while (QTime::currentTime() < dieTime)
+      QCoreApplication::processEvents(QEventLoop::AllEvents,
+				      MOUSE_BUTTON_DELAY);
+    if (mousePressActive)
+      timer->start (MOUSE_BUTTON_DELAY);
+  }
+  else
+    QAbstractSpinBox::mousePressEvent(mouseEvent);
+}
+
+void SciDoubleSpinBox::setValue (double v)
+{
+  value = v;
+  this->lineEdit ()->setText (QString::number (value));
+}
+
+bool SciDoubleSpinBox::eventFilter(QObject *object, QEvent *event)
+{
+  if (object == this && event->type() == QEvent::MouseButtonRelease) {
+    //    QMouseEvent * mouseEvent = static_cast<QMouseEvent *>(event);
+    mousePressActive = false;
+    timer->stop ();
+  }
+  return false;
+}
+
+SciDoubleSpinBox::~SciDoubleSpinBox ()
+{
+}
+
---
+++ scidoublespinbox.h
@@ -0,0 +1,43 @@
+#ifndef SCIDOUBLESPINBOX_H
+#define SCIDOUBLESPINBOX_H
+
+#include <QtWidgets>
+
+#include "enums.h"
+
+class
+SciDoubleSpinBox : public QAbstractSpinBox
+{
+    Q_OBJECT
+
+public:
+    typedef enum {
+      Min,
+      Max
+    } SpinType;
+
+    SciDoubleSpinBox (SpinType t, QWidget *parent = nullptr);
+    ~SciDoubleSpinBox ();
+    double getValue ();
+    void setValue (double v);
+    void setLockBox (QCheckBox *lb) { lockBox = lb; }
+    //    double clampValue (double v);
+
+ Q_SIGNALS:
+    void valueChanged ();
+    
+protected:
+    void keyPressEvent(QKeyEvent *event);
+    void mousePressEvent(QMouseEvent *event);
+    bool eventFilter(QObject *object, QEvent *event);
+    
+ private:
+    double value;
+    QCheckBox *lockBox;
+    SpinType stype;
+    bool mousePressActive;
+    QTimer *timer;
+    double incr;
+};
+
+#endif // SCIDOUBLESPINBOX_H
